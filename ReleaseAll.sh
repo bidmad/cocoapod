24786c06fe7a8fcab7fca68b5f219c3e5f05c39b
MarkusOhADOP <83572705+MarkusOhADOP@users.noreply.github.com>
2022-08-09 12:33:49 +0900

    ReleaseAll Shell Script Added

---
+++ ReleaseAll.sh
@@ -0,0 +1,16 @@
+BASEDIR=$(dirname "$0")
+
+pod trunk push $BASEDIR/ADOPUtility.podspec --allow-warnings --skip-import-validation
+pod trunk push $BASEDIR/BidmadAdapterFC.podspec --allow-warnings --skip-import-validation
+
+while [ $? != 0 ]
+do
+pod repo update
+echo "Retrying in 5 minutes"
+sleep 300
+pod trunk push $BASEDIR/BidmadAdapterFC-Dev.podspec --allow-warnings --skip-import-validation
+done
+
+pod trunk push $BASEDIR/BidmadAdapterFNC.podspec --allow-warnings --skip-import-validation
+pod trunk push $BASEDIR/BidmadSDK.podspec --allow-warnings --skip-import-validation
+pod trunk push $BASEDIR/OpenBiddingHelper.podspec --allow-warnings --skip-import-validation
