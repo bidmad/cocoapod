6feb33c083397cc796ce77e8f336ce29af788749
MarkusOhADOP <83572705+MarkusOhADOP@users.noreply.github.com>
2022-11-07 11:57:00 +0900

    Shell Scripts for Releasing ADOP Frameworks

---
+++ ReleaseFC.sh
@@ -0,0 +1,2 @@
+BASEDIR=$(dirname "$0")
+pod trunk push $BASEDIR/BidmadAdapterFC.podspec --allow-warnings --skip-import-validation
---
+++ ReleaseSDK.sh
@@ -0,0 +1,2 @@
+BASEDIR=$(dirname "$0")
+pod trunk push $BASEDIR/BidmadSDK.podspec --allow-warnings --skip-import-validation
