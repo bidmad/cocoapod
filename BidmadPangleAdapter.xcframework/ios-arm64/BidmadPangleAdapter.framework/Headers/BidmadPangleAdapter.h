211b0d09f4379ebb3ce11365f1f2d227c80728e0
MarkusOhADOP <83572705+MarkusOhADOP@users.noreply.github.com>
2023-10-18 10:17:59 +0900

    Pangle 어댑터 배포 (5.3.1.0.0)

---
+++ BidmadPangleAdapter.xcframework/ios-arm64/BidmadPangleAdapter.framework/Headers/BidmadPangleAdapter.h
@@ -0,0 +1,18 @@
+//
+//  BidmadPangleAdapter.h
+//  BidmadPangleAdapter
+//
+//  Created by 플랫폼-오승섭-맥북 on 2023/08/29.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BidmadPangleAdapter.
+FOUNDATION_EXPORT double BidmadPangleAdapterVersionNumber;
+
+//! Project version string for BidmadPangleAdapter.
+FOUNDATION_EXPORT const unsigned char BidmadPangleAdapterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BidmadPangleAdapter/PublicHeader.h>
+
+
