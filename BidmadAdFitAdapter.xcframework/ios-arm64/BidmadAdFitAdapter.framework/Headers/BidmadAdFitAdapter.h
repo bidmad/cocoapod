60ac4d7e931d7c78b59c970c304bfc9a9ce7597a
MarkusOhADOP <83572705+MarkusOhADOP@users.noreply.github.com>
2023-10-18 14:30:52 +0900

    AdFit 어댑터 배포

---
+++ BidmadAdFitAdapter.xcframework/ios-arm64/BidmadAdFitAdapter.framework/Headers/BidmadAdFitAdapter.h
@@ -0,0 +1,18 @@
+//
+//  BidmadAdFitAdapter.h
+//  BidmadAdFitAdapter
+//
+//  Created by 플랫폼-오승섭-맥북 on 2023/10/10.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BidmadAdFitAdapter.
+FOUNDATION_EXPORT double BidmadAdFitAdapterVersionNumber;
+
+//! Project version string for BidmadAdFitAdapter.
+FOUNDATION_EXPORT const unsigned char BidmadAdFitAdapterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BidmadAdFitAdapter/PublicHeader.h>
+
+
