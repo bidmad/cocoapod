e00a4ebea6ee6ed49dac4aa5351b217b542c6ccc
MarkusOhADOP <83572705+MarkusOhADOP@users.noreply.github.com>
2023-10-18 11:02:41 +0900

    Fyber / Vungle 어댑터 프레임워크 추가

---
+++ BidmadFyberAdapter.xcframework/ios-arm64/BidmadFyberAdapter.framework/Headers/BidmadFyberAdapter.h
@@ -0,0 +1,18 @@
+//
+//  BidmadFyberAdapter.h
+//  BidmadFyberAdapter
+//
+//  Created by 플랫폼-오승섭-맥북 on 2023/08/30.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BidmadFyberAdapter.
+FOUNDATION_EXPORT double BidmadFyberAdapterVersionNumber;
+
+//! Project version string for BidmadFyberAdapter.
+FOUNDATION_EXPORT const unsigned char BidmadFyberAdapterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BidmadFyberAdapter/PublicHeader.h>
+
+
---
+++ BidmadVungleAdapter.xcframework/ios-x86_64-simulator/BidmadVungleAdapter.framework/Headers/BidmadVungleAdapter.h
@@ -0,0 +1,18 @@
+//
+//  BidmadVungleAdapter.h
+//  BidmadVungleAdapter
+//
+//  Created by 플랫폼-오승섭-맥북 on 2023/08/30.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BidmadVungleAdapter.
+FOUNDATION_EXPORT double BidmadVungleAdapterVersionNumber;
+
+//! Project version string for BidmadVungleAdapter.
+FOUNDATION_EXPORT const unsigned char BidmadVungleAdapterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BidmadVungleAdapter/PublicHeader.h>
+
+
