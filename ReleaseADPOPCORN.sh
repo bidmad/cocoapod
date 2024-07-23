f3e86c0f8c845d65279aadbc1afc696e45493f67
MarkusOhADOP <83572705+MarkusOhADOP@users.noreply.github.com>
2024-07-23 17:11:58 +0900

    BidmadSDK Adapter Call Fix

---
+++ ReleaseADPOPCORN.sh
@@ -0,0 +1,2 @@
+BASEDIR=$(dirname "$0")
+pod trunk push $BASEDIR/BidmadAdPopcornAdapter.podspec --allow-warnings --skip-import-validation
---
+++ ReleaseALLIANCE.sh
@@ -0,0 +1,2 @@
+BASEDIR=$(dirname "$0")
+pod trunk push $BASEDIR/BidmadAllianceInternetAdapter.podspec --allow-warnings --skip-import-validation
